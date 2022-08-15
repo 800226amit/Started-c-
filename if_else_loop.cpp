75d4f7937baf31ae8f120641b595c5389480cfcc
Amit Kumar <89442730+800226amit@users.noreply.github.com>
2022-08-15 11:32:29 +0530

    if, else,  else-if statement
    if,  else, statement using c++

---
+++ if_else_loop.cpp
@@ -0,0 +1,36 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int num;
+//     char ch;
+//     cout<<"Enter your Value";
+//     cin >> ch;
+    
+//     // cin >>num;
+//      if(ch>='A' && ch<='Z'){
+//         cout << "The character is upper case :-" << ch;
+//      }
+//      else if(ch>='a' && ch<='z'){
+//         cout << "The character is lower case:-" <<ch;
+//      }
+//      else{
+//         cout<<"it is number:- " <<ch;
+//      }
+// }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    
+
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        cout<< i << " ";
+    }
+
+    
+}
